a332f37db1e642ffaa8812733d83d0dbef4038e8
lekvispk <lekvispk@07eb1019-d1f2-588c-5852-46132ad1842e>
2012-02-06 21:35:57 +0000

    test

--- trunk/constitucion/test/pe/gob/pcm/constitucion/web/service/ParticipanteTest.java
+++ trunk/constitucion/test/pe/gob/pcm/constitucion/web/service/ParticipanteTest.java
@@ -1,5 +1,19 @@
 package pe.gob.pcm.constitucion.web.service;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/applicationContext-test.xml")
 public class ParticipanteTest {
 
+	@Test
+	public void testObtenerVuelos() {
+		Assert.assertTrue( 1 == 1 );
+	}
+	
+	
 }
