8155b3c3e75f6530de26a33cd74a6ab7386438cb
lekvispk <lekvispk@07eb1019-d1f2-588c-5852-46132ad1842e>
2012-04-23 23:32:19 +0000

    cambio en el login controller para verificar accesos

--- demo/src/main/java/pe/gob/mininter/controller/LoginController.java
+++ demo/src/main/java/pe/gob/mininter/controller/LoginController.java
@@ -27,33 +27,36 @@ public class LoginController  {
 	
 	@RequestMapping("/login.htm")
     public String login(ModelMap model , HttpServletRequest request) throws NumberFormatException, Exception {
-		
-		//String username = request.getParameter("id").trim();
-		//no poner trim ya que si no llega el parametro id, devuelve null, y null.trim da nullpointerexception
-		
-		String username = request.getParameter("id");
-		
-		if(username != null){
-			username = username.trim();//aqui si puedo hacer trim de forma segura
-			System.out.println("intento de logueo = " +username);
-			SiminMaestro usuario = (SiminMaestro) usuarioService.loadUserByUsername(username);	
-				
-			InetAddress thisIp = InetAddress.getLocalHost();
-			thisIp.getHostAddress().toString();
-			//String  thisIpAddress =  request.getRemoteAddr(); //--> request.getRemoteAddr(); trae la ip de quien ha llamado a esta pagina 
-			usuario.setIpPrivada(thisIp.getHostAddress().toString());
+		try {			
+			//String username = request.getParameter("id").trim();
+			//no poner trim ya que si no llega el parametro id, devuelve null, y null.trim da nullpointerexception
 			
-
-			request.getSession().setAttribute("usuario", usuario);
-			request.getSession().setAttribute("lstSistemas", usuarioService.listarSistemas(username) );
-			request.getSession().setAttribute("lcumpleanios", usuarioService.listarCumpleaniosMes("","") );
-			return "/home";	
-		}else{
-			//si es que no he podido obtener el username muestro pantalla de login
-			model.put("msgError", "no se ha encontrado el parametro username");
-			return "/home";
+			String username = request.getParameter("id");
+			
+			if(username != null){
+				username = username.trim();//aqui si puedo hacer trim de forma segura
+				System.out.println("intento de logueo = " +username);
+				SiminMaestro usuario = (SiminMaestro) usuarioService.loadUserByUsername(username);	
+					
+				InetAddress thisIp = InetAddress.getLocalHost(); // esto obtiene que IP? esto no obtiene la IP del servidor en donde corre esta aplicacion? 
+				//String  thisIpAddress = thisIp.getHostAddress().toString();
+				System.out.println("IP DE REQUEST " + request.getRemoteAddr() ); //Soplo cuando consultan desde la calle, tal vez IP EXTERNA 
+				//String  thisIpAddress =  request.getRemoteAddr(); //--> request.getRemoteAddr(); trae la ip de quien ha llamado a esta pagina 
+				usuario.setIpPrivada(thisIp.getHostAddress().toString());
+					
+				request.getSession().setAttribute("usuario", usuario);
+				request.getSession().setAttribute("lstSistemas", usuarioService.listarSistemas(username) );
+				request.getSession().setAttribute("lcumpleanios", usuarioService.listarCumpleaniosMes("","") );
+				return "/home";	
+			}else{
+				//si es que no he podido obtener el username muestro pantalla de login
+				model.put("msgError", "no se ha encontrado el parametro username");
+				return "/login";
+			}
+		} catch (Exception e) {
+			model.put("msgError", "ERROR: " + e.getMessage());
+			return "/login";
 		}
-
     }
 	
 	@RequestMapping("/home.htm")
