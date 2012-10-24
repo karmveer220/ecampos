55acb94d3e0de90f948ea9a935e336a6e54a33fc
lekvispk <lekvispk@07eb1019-d1f2-588c-5852-46132ad1842e>
2012-10-24 16:08:25 +0000

    agregado QUARTZ

---
+++ demo/src/main/java/pe/gob/mininter/utiles/Proceso.java
@@ -0,0 +1,16 @@
+package pe.gob.mininter.utiles;
+
+/**
+ * Clase que contiene a el/los metodos que se ejecutaran automaticamente
+ * @author ECAMPOS
+ *
+ */
+public class Proceso {
+
+	public void ejecutarProceso() {
+
+		System.out.println("se esta ejecutando automaticamente");
+		System.out.println("aqui crea o llama a tu clase que ejecuta tu proceso automatico");
+		
+	}
+}
