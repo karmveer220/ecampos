1706a83d7153ef6de0a8d63bb4134b5715e8ab14
lekvispk <lekvispk@07eb1019-d1f2-588c-5852-46132ad1842e>
2014-04-14 06:58:01 +0000

    graba

---
+++ trunk/sellos_cnc/src/pe/org/cnc/model/Usuario.java
@@ -0,0 +1,80 @@
+package pe.org.cnc.model;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+
+/**
+ * The persistent class for the usuario database table.
+ * 
+ */
+@Entity
+@NamedQuery(name="Usuario.findAll", query="SELECT u FROM Usuario u")
+public class Usuario implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private int idUsuario;
+
+	private String apellidos;
+
+	private String clave;
+
+	private int estado;
+
+	private String nombres;
+
+	private String username;
+
+	public Usuario() {
+	}
+
+	public int getIdUsuario() {
+		return this.idUsuario;
+	}
+
+	public void setIdUsuario(int idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+
+	public String getApellidos() {
+		return this.apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public String getClave() {
+		return this.clave;
+	}
+
+	public void setClave(String clave) {
+		this.clave = clave;
+	}
+
+	public int getEstado() {
+		return this.estado;
+	}
+
+	public void setEstado(int estado) {
+		this.estado = estado;
+	}
+
+	public String getNombres() {
+		return this.nombres;
+	}
+
+	public void setNombres(String nombres) {
+		this.nombres = nombres;
+	}
+
+	public String getUsername() {
+		return this.username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+}
---
+++ trunk/sellos_cnc/src/pe/org/cnc/service/impl/NotarioServiceImpl.java
@@ -0,0 +1,36 @@
+package pe.org.cnc.service.impl;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pe.org.cnc.dao.NotarioDAO;
+import pe.org.cnc.model.Notario;
+import pe.org.cnc.service.NotarioService;
+
+@Service
+public class NotarioServiceImpl implements NotarioService {
+
+	@Autowired
+	private NotarioDAO notarioDAO;
+	
+	@Override
+	public void registrarNotario(Notario notario) {
+		this.notarioDAO.registrarNotario(notario);
+	}
+
+	@Override
+	public Notario obtenerNotario(Integer idNotario) {
+		return this.notarioDAO.obtenerNotario(idNotario);
+	}
+
+	@Override
+	public void eliminarNotario(Integer idNotario) {
+		this.notarioDAO.eliminarNotario(idNotario);
+	}
+
+	@Override
+	public List<Notario> buscarNotario(Notario notario) {
+		return this.notarioDAO.buscarNotario(notario);
+	}
+
+}
